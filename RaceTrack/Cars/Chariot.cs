614f00054b3205d6b729f90cb9901193f8e0905e
epeppin <epeppin@my.wctc.edu>
2022-03-07 18:37:18 -0600

    Added Driver, RaceCar and implemented StopEngine

---
+++ RaceTrack/Cars/Chariot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Chariot : RaceCar
+    {
+        public Chariot()
+        {
+            Name = "Chariot";
+            TopSpeed = 20;
+        }
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} gallops to the starting line!");
+        }
+        public override void StopEngine()
+        {
+            Console.WriteLine($"The {Name} gallops to a stop");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Cesar.cs
@@ -0,0 +1,18 @@
+﻿using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Cesar : Driver
+    {
+        public Cesar(RaceCar car) : base(car)
+        {
+            Name = "Cesar";
+            SkillLevel = 9;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
